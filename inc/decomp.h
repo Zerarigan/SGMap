ae0aef2fa485035553e7758a5c5331e2850c7b33
tanjmaxalb <tanjmaxalb@gmail.com>
2016-09-08 00:36:15 +0200

    add decomperion functions

---
+++ inc/decomp.h
@@ -0,0 +1,12 @@
+#ifndef DECOMP_H
+#define DECOMP_H
+
+// Markers
+#define MARK		0xFF
+#define COMMENT		0xFE
+#define DQT			0xDB	// Quatization Table
+#define SOF0		0xC0	// Basic encode method. Baseline DCT
+#define DHT			0xC4	// Haffman's Table
+#define SOS			0xDA	// Start of Scan
+
+#endif 
